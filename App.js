64323db4564255c5b2586e7bf1ffc237f50792e9
Parth Koshta <parth.k@theghack.in>
2019-12-26 13:04:20 +0530

    ui update

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
-import TRUECALLER, {
-  TRUECALLER_EVENT,
-  TRUECALLER_CONSENT_MODE,
-  TRUECALLER_CONSENT_TITLE,
-  TRUECALLER_FOOTER_TYPE,
-} from 'react-native-truecaller-sdk';
+import TRUECALLER, {TRUECALLER_EVENT, TRUECALLER_CONSENT_MODE, TRUECALLER_CONSENT_TITLE} from 'react-native-truecaller-sdk';
 
 class App extends Component {
   componentDidMount() {
-    TRUECALLER.initializeClient();
+    TRUECALLER.initializeClient(
+      TRUECALLER_CONSENT_MODE.Popup,
+      TRUECALLER_CONSENT_TITLE.Login
+    );
     TRUECALLER.isUsable(result => {
       if (result === true) {
         console.log('Authenticate via truecaller flow can be used');
@@ -25,6 +23,8 @@ class App extends Component {
     TRUECALLER.requestTrueProfile();
     TRUECALLER.on(TRUECALLER_EVENT.TrueProfileResponse, profile => {
       console.log('Truecaller profile data: ', profile);
+      let details = `${profile.firstName}, ${profile.email}, ${profile.phoneNumber}`
+      alert(details);
       // add other logic here related to login/sign-up as per your use-case.
     });
 
@@ -35,10 +35,15 @@ class App extends Component {
 
   render() {
     return (
-      <View>
+      <View
+        style={{
+          backgroundColor: '#1180FF',
+        }}>
         <Text
           style={{
             textAlign: 'center',
+            color: '#fff',
+            fontSize: 22,
           }}>
           Truecaller
         </Text>
@@ -49,8 +54,14 @@ class App extends Component {
             justifyContent: 'center',
             alignItems: 'center',
           }}>
-          <TouchableOpacity onPress={this.loginViaTruecaller}>
-            <Text>Login via truecaller</Text>
+          <TouchableOpacity onPress={this.loginViaTruecaller} style={{
+            padding: 10,
+            backgroundColor: "#fff",
+            elevation: 2
+          }}>
+            <Text style={{color: '#1180FF', fontSize: 22}}>
+              Login via truecaller
+            </Text>
           </TouchableOpacity>
         </View>
       </View>
